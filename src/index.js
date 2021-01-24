71ec02c86db879adfad2fcaa039aaa0a0fc61330
sergJess <serg.fantasy@yandex.ru>
2021-01-24 11:35:49 +0300

    feat: add solution for task typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (Array.isArray(array)) {
+        const length = array.length;
+        if (length == 0) return 0;
+        const sortedArray = quickSortArray(array);
+        const minValue = sortedArray[0]
+        return minValue;
+    }
+    return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (Array.isArray(array)) {
+        const length = array.length;
+        if (length == 0) return 0;
+        const sortedArray = quickSortArray(array);
+        const maxValue = sortedArray[length - 1];
+        return maxValue;
+    }
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (Array.isArray(array)) {
+        const length = array.length;
+        if (length == 0) return 0;
+        const averageValue = (array.reduce((accum, index) => accum + index)) / length;
+        return averageValue;
+    }
+    return 0;
+}
+
+function quickSortArray(array) {
+    const length = array.length;
+    if (length < 2) return array;
+    let pivot = array[0];
+    const leftArray = [];
+    const rightArray = [];
+    for (let i = 1; i < length; i++) {
+        (pivot > array[i]) ? leftArray.push(array[i]): rightArray.push(array[i]);
+    }
+    return quickSortArray(leftArray).concat(pivot, quickSortArray(rightArray));
+
 }
